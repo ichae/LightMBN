6d3acc2fcf7ec510dfddf57594c2f5dbfe44d49d
Cho, Hyunwoo <hwcho@ust.ac.kr>
2022-04-26 16:14:46 +0900

    args -> cfg

--- main.py
+++ main.py
@@ -29,35 +29,30 @@ cfg.merge_from_list(args.opts)
 cfg.freeze()
 
 #
-if args.config != '':
-    with open(args.config, 'r') as f:
-        config = yaml.load(f)
-    for op in config:
-        setattr(args, op, config[op])
 torch.backends.cudnn.benchmark = True
 
-# loader = data.Data(args)
-ckpt = utility.checkpoint(args)
-loader = data_v2.ImageDataManager(args)
-model = make_model(args, ckpt)
-optimzer = make_optimizer(args, model)
-loss = make_loss(args, ckpt) if not args.test_only else None
+# loader = data.Data(cfg)
+ckpt = utility.checkpoint(cfg)
+loader = data_v2.ImageDataManager(cfg)
+model = make_model(cfg, ckpt)
+optimzer = make_optimizer(cfg, model)
+loss = make_loss(cfg, ckpt) if not args.test_only else None
 
 start = -1
-if args.load != '':
+if cfg.load != '':
     start, model, optimizer = ckpt.resume_from_checkpoint(
         osp.join(ckpt.dir, 'model-latest.pth'), model, optimzer)
     start = start - 1
-if args.pre_train != '':
-    ckpt.load_pretrained_weights(model, args.pre_train)
+if cfg.pre_train != '':
+    ckpt.load_pretrained_weights(model, cfg.pre_train)
 
-scheduler = make_scheduler(args, optimzer, start)
+scheduler = make_scheduler(cfg, optimzer, start)
 
 # print('[INFO] System infomation: \n {}'.format(get_pretty_env_info()))
-ckpt.write_log('[INFO] Model parameters: {com[0]} flops: {com[1]}'.format(com=compute_model_complexity(model, (1, 3, args.height, args.width))
+ckpt.write_log('[INFO] Model parameters: {com[0]} flops: {com[1]}'.format(com=compute_model_complexity(model, (1, 3, cfg.height, cfg.width))
                                                                           ))
 
-engine = engine_v3.Engine(args, model, optimzer,
+engine = engine_v3.Engine(cfg, model, optimzer,
                           scheduler, loss, loader, ckpt)
 # engine = engine.Engine(args, model, loss, loader, ckpt)
 
@@ -66,7 +61,7 @@ while not engine.terminate():
 
     n += 1
     engine.train()
-    if args.test_every != 0 and n % args.test_every == 0:
+    if cfg.test_every != 0 and n % cfg.test_every == 0:
         engine.test()
-    elif n == args.epochs:
+    elif n == cfg.epochs:
         engine.test()
